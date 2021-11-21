abe3ce6fc0b02fee40b90096dfedd2e2f5d19cbb
劉宜婕 <jacvd7@gmail.com>
2021-11-22 04:44:36 +0800

    feat: add tutorial

--- src/pages/CounterReducer/CounterReducer.tsx
+++ src/pages/CounterReducer/CounterReducer.tsx
@@ -2,28 +2,12 @@ import React, { useState, useReducer } from 'react'
 
 import { Page, PageContent } from '@components/Page'
 
-const ACTION = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement',
-  RESET: 'reset',
-}
-
 const initialCount = 0
 
-function init(initialCnt) {
-  return { count: initialCnt }
-}
+function init(initialCnt) {}
 
 function reducer(state, action) {
   switch (action.type) {
-    case ACTION.INCREMENT:
-      return { count: state.count + 1 }
-    case ACTION.DECREMENT:
-      return { count: state.count - 1 }
-    case ACTION.RESET:
-      return init(action.payload)
-    default:
-      return state
   }
 }
 
@@ -31,25 +15,18 @@ const CounterReducer: React.FC = () => {
   // const [count, setCount] = useState(0)
   const [state, dispatch] = useReducer(reducer, initialCount, init)
 
-  const increment = () => {
-    dispatch({ type: ACTION.INCREMENT })
-  }
+  const increment = () => {}
 
-  const decrement = () => {
-    dispatch({ type: ACTION.DECREMENT })
-  }
+  const decrement = () => {}
 
-  const reset = () => {
-    dispatch({ type: ACTION.RESET, payload: initialCount })
-  }
+  const reset = () => {}
 
-  console.log(state)
   return (
     <>
       <Page>
         <PageContent>
           <button onClick={decrement}>-</button>
-          <span>{state.count}</span>
+          <span>{0}</span>
           <button onClick={increment}>+</button>
           <br />
           <button onClick={reset}>RESET</button>
--- src/pages/Todolist/Todo.tsx
+++ src/pages/Todolist/Todo.tsx
@@ -1,19 +1,4 @@
 import React from 'react'
-import { ACTION } from './Todolist'
-export const Todo = ({ todo, dispatch }) => {
-  console.log('todo', todo.todoName)
-  return (
-    <>
-      <div>
-        <span style={{ color: todo.complete ? 'green' : '#000' }}>{todo.todoName}</span>
-        <button onClick={() => dispatch({ type: ACTION.TOGGLE_TODO, payload: { id: todo.id } })}>
-          TOGGLE
-        </button>
-        <button onClick={() => dispatch({ type: ACTION.DELETE_TODO, payload: { id: todo.id } })}>
-          DELETE
-        </button>
-      </div>
-      <br />
-    </>
-  )
+export const Todo = ({}) => {
+  return <></>
 }
