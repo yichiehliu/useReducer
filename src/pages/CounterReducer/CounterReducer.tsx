b30be91846a55c025f76291945d4b5d3a60bd263
劉宜婕 <jacvd7@gmail.com>
2021-11-22 05:03:46 +0800

    fix: remove imports

--- src/pages/CounterReducer/CounterReducer.tsx
+++ src/pages/CounterReducer/CounterReducer.tsx
@@ -1,35 +1,22 @@
-import React, { useState, useReducer } from 'react'
+import React, { useState } from 'react'
 
 import { Page, PageContent } from '@components/Page'
 
-const initialCount = 0
-
-function init(initialCnt) {}
-
-function reducer(state, action) {
-  switch (action.type) {
-  }
-}
-
 const CounterReducer: React.FC = () => {
-  // const [count, setCount] = useState(0)
-  const [state, dispatch] = useReducer(reducer, initialCount, init)
+  const [count, setCount] = useState(0)
 
   const increment = () => {}
 
   const decrement = () => {}
-
-  const reset = () => {}
 
   return (
     <>
       <Page>
         <PageContent>
           <button onClick={decrement}>-</button>
-          <span>{0}</span>
+          <span>{count}</span>
           <button onClick={increment}>+</button>
           <br />
-          <button onClick={reset}>RESET</button>
         </PageContent>
       </Page>
     </>
